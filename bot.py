af8519c376bd07235b9499854653236865e8100e
Sharven Prasad Dhanasekar <sharven.dhanasekar@mail.utoronto.ca>
2021-04-23 12:45:09 -0400

    Implement job scheduling to send attendance message at the scheduled time

--- bot.py
+++ bot.py
@@ -5,6 +5,9 @@ from discord.ext import commands
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+import asyncio
 """
 
 ATTENDANCE BOT 
@@ -30,7 +33,10 @@ bot = commands.Bot(command_prefix='!')
 connection = MongoClient(os.getenv('DB_URL'))
 db = connection['discordBot']
 debug = db['debug']
+cwd = os.getcwd()
 
+sched = AsyncIOScheduler()
+sched.start()
 
 @bot.event
 async def on_ready():
@@ -90,10 +96,19 @@ async def set_preferences(ctx, _time, _points, _message_to_send):
         user = ctx.author.id
         server = ctx.guild.id
         collection = db[str(server)]
-        collection.update_one({'time':alarm}, {"$set" : {"time":alarm, "points":p, "message": _message_to_send}}, upsert=True)
+        # collection.update_one({'time': alarm}, {"$set": {"time": alarm, "points": p, "message": _message_to_send}}, upsert=True)
+        
+        collection.update_one({'time': alarm}, {"$set": {"time": alarm, "points": p, "message": _message_to_send}}, upsert=True)
+        document = collection.find_one({'time': alarm})
+        # Create job
+        sched.add_job(send_attendance_message, 'cron', hour=alarm[:2], minute=alarm[3:5], id=str(document['_id']), args=(ctx, _message_to_send))
+        
         await ctx.send(f"alarm set for {alarm}, worth {p} points!")
     except:
         await ctx.send("looks like that command wasn't formatted correctly, use `bot_help` to find the correct way to set up this bot")
+
+async def send_attendance_message(ctx, _message_to_send):
+    await ctx.send(_message_to_send)
 
 @bot.command(name='delete_alarm')
 async def debug_time(ctx, _time):
@@ -103,7 +118,11 @@ async def debug_time(ctx, _time):
     try:
         alarm = time.strptime(_time, "%H:%M")
         alarm = time.strftime("%H:%M", alarm)
-        collection.delete_one({"time":alarm})
+        document = collection.find_one({"time": alarm})
+        collection.delete_one({"time": alarm})
+        
+        sched.remove_job(str(document['_id']))
+
         await ctx.send(f"deleted alarm for {alarm}")
     except:
         return await ctx.send("not a valid time format")
@@ -114,6 +133,8 @@ async def info(ctx):
     await ctx.send("TO SET UP THIS BOT USE THE `set_alarm` command, arguments are space seperated \n <time> -> HH:mm string for when to send (24 hour time)\n <points> -> int how many points this rollcall is worth\n <message_to_send> -> str the message the bot will send ")
 
 bot.run(TOKEN)
+asyncio.get_event_loop().run_forever()
+
 # client = discord.Client()
 
 # @client.event
